99b92a71a64b50fecdf52192fb0ea7d1f333ed3c
clhore <79727938+clhore@users.noreply.github.com>
2021-09-12 17:25:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# library
+import platform
 
 # module file
 from module import pdf, JSON, auto_intagram
@@ -8,30 +10,27 @@ if __name__ == '__main__':
     credentials = JSON.JSON(file='instagram_credentials.json').read()
     # import content system.json
     system = JSON.JSON(file='system.json').read()
-    
-    # check user system
-    if system['system'].lower() == 'linux':
-        driver_system = 'chromedriver'
-        path_bar = '//'
-    elif system['system'].lower() == 'windows':
+
+    # check your system
+    if platform.system().lower() == "windows":
         driver_system = 'chromedriver.exe'
         path_bar = '\\'
     else:
-        driver_system = 'chromedriver.exe'
-        path_bar = '\\'
+        driver_system = 'chromedriver'
+        path_bar = '//'
 
     # create new session auto instagram
     instagram = auto_intagram.INSTAGRAM(driver_path=driver_system, path_bar=path_bar)
 
     # options and control
-    instagram.open() # open instagram
-    instagram.login(username=credentials['username'], password=credentials['password']) # login
-    instagram.search(username='el_pais', description='EL PAÍS') # search an select user
-    instagram.post(num=2) # extract post
+    instagram.open()  # open instagram
+    instagram.login(username=credentials['username'], password=credentials['password'])  # login
+    instagram.search(username='el_pais', description='EL PAÍS')  # search an select user
+    instagram.post(num=2)  # extract post
     instagram.extract_img_and_text(ruta='article')
-    instagram.close() # close driver
+    instagram.close()  # close driver
 
-    # create file PDF 
+    # create file PDF
     pdf_file = pdf.PDF(orientation='L')  # pdf object
 
     # create page article
@@ -40,7 +39,7 @@ if __name__ == '__main__':
     for name_art in list_img:
         # create new page
         pdf_file.add_page(orientation='L')
-        pdf_file.flag_es() # and flag from spain
+        pdf_file.flag_es()  # and flag from spain
         # select file
         _file = 'article{0}{1}'.format(path_bar, name_art)
         pdf_file.picture(file=_file, x=20, y=20.0, w=1586 / 15, h=1920 / 15)
